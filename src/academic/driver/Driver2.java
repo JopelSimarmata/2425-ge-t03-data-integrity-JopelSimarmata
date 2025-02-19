cbb59943f6548cec926673736b1ddf59f41b5462
Jopel Simarmata <iss23036@students.del.ac.id>
2025-02-19 10:50:02 +0700

    UPDATE

--- src/academic/driver/Driver2.java
+++ src/academic/driver/Driver2.java
@@ -7,8 +7,8 @@ import academic.model.Student;
 import academic.model.Enrollment;
 
 /**
- * @autor 12S23036 Jopel Simarmata
- * @autor 12S23024 Eska Silaen
+ * @author 12S23036 Jopel Simarmata
+ * @author 12S23024 Eska Silaen
  */
 public class Driver2 {
 
@@ -18,20 +18,27 @@ public class Driver2 {
         Boolean cek_s = false;
         Boolean cek_c = false;
 
+
+        
         ArrayList<Course> daftarMataKuliah = new ArrayList<>();
         ArrayList<Student> daftarMahasiswa = new ArrayList<>();
         ArrayList<Enrollment> daftarEnrollments = new ArrayList<>();
 
         ArrayList<String> invalid = new ArrayList<>();
+        ArrayList<String> invalid2 = new ArrayList<>();
+ 
 
-        while (true) {
+
+
+
+        while(true){
             String command = scn.nextLine();
 
-            if (command.equalsIgnoreCase("---")) {
+            if(command.equalsIgnoreCase("---")){
                 break;
-            }
+            } 
             String[] temp = new String[5];
-            // split command
+            //split command
             String[] splitCommand = command.split("#");
 
             temp[0] = splitCommand[0];
@@ -40,67 +47,93 @@ public class Driver2 {
             temp[3] = splitCommand[3];
             temp[4] = splitCommand[4];
 
-            if (temp[0].equals("course-add")) {
+            if(temp[0].equals("course-add")){
                 Course course = new Course(temp[1], temp[2], temp[3], temp[4]);
                 daftarMataKuliah.add(course);
 
-            } else if (temp[0].equals("student-add")) {
+            } else if(temp[0].equals("student-add")){
                 Student student = new Student(temp[1], temp[2], temp[3], temp[4]);
                 daftarMahasiswa.add(student);
 
-            } else if (temp[0].equals("enrollment-add")) {
-                cek_s = false;
-                cek_c = false;
+            } else if(temp[0].equals("enrollment-add")){
 
-                for (int i = 0; i < daftarMahasiswa.size(); i++) {
-                    if (daftarMahasiswa.get(i).getNim().equals(temp[2])) {
+                for (int i = 0; i < daftarMahasiswa.size(); i++){
+                    //jika tidak ada daftarmahasiswa
+                    if (daftarMahasiswa.size() == 0){
+                        invalid.add("invalid student|" + temp[2]);
+                        invalid2.add(temp[2]);
+                        break;
+                    }
+
+                    else if (daftarMahasiswa.get(i).getNim().equals(temp[2])){
                         cek_s = true;
                         break;
-                    }
-                }
-
-                for (int i = 0; i < daftarMataKuliah.size(); i++) {
-                    if (daftarMataKuliah.get(i).getCodeCourse().equals(temp[1])) {
-                        cek_c = true;
+                    } else {
+                        invalid.add("invalid student|" + temp[2]);
+                        invalid2.add(temp[2]);
                         break;
                     }
                 }
 
-                if (!cek_s) {
-                    invalid.add("invalid student|" + temp[2]);
+                for (int i = 0; i < daftarMataKuliah.size(); i++){
+                    if (daftarMataKuliah.get(i).getCodeCourse().equals(temp[1])){
+                        cek_c = true;
+                        break;
+                    } else {
+                        invalid.add("invalid course|" + temp[1]);
+                        invalid2.add(temp[1]);
+                        break;
+                    }
                 }
 
-                if (!cek_c) {
-                    invalid.add("invalid course|" + temp[1]);
-                }
-
-                if (cek_s && cek_c) {
+                //cek agar enrolment tidak di add jika salah satu student atau course tidak valid
+                if (cek_s == true && cek_c == true){
                     Enrollment enrollment = new Enrollment(temp[1], temp[2], temp[3], temp[4]);
                     daftarEnrollments.add(enrollment);
+                } else {
+                    continue;
                 }
+
             }
         }
 
-        // print invalid
-        for (int i = 0; i < invalid.size(); i++) {
-            for (int j = i + 1; j < invalid.size(); j++) {
-                if (invalid.get(i).equals(invalid.get(j))) {
+
+
+        //print invalid
+        for (int i = 0; i < invalid.size(); i++){
+            for (int j = i+1; j < invalid.size(); j++){
+                if (invalid.get(i).equals(invalid.get(j))){
                     invalid.remove(j);
                 }
             }
             System.out.println(invalid.get(i));
         }
 
-        for (int i = daftarMataKuliah.size() - 1; i >= 0; i--) {
+
+
+        for(int i = daftarMataKuliah.size()-1; i >= 0; i--){
             daftarMataKuliah.get(i).DisplayCourse();
         }
 
-        for (int i = 0; i < daftarMahasiswa.size(); i++) {
+        for(int i = 0; i < daftarMahasiswa.size(); i++){
             daftarMahasiswa.get(i).DisplayStudent();
         }
 
-        for (Enrollment enrollment : daftarEnrollments) {
-            enrollment.DisplayEnrollment();
+        //print yang valid saja menggunakan invalid2 sebagai pengecualian 
+
+        for(int i = 0; i < daftarEnrollments.size(); i++){
+            if (invalid2.contains(daftarEnrollments.get(i).getNim()) || invalid2.contains(daftarEnrollments.get(i).getCodeCourse())){
+                continue;
+            } else {
+                daftarEnrollments.get(i).DisplayEnrollment();
+                break;
+            }
         }
+
+        
+
+        // codes
+
     }
+
 }
